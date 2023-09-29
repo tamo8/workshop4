91bcbb4ad7f2e15be9fd2724bb54223db9df9ba5
tamo8 <141841268+tamo8@users.noreply.github.com>
2023-09-29 22:51:51 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+/*1 
+let arr= [5, 25, 89, 120, 36]; 
+arr.push("javascript", "python");
+arr.unshift('html', 'css');
+console.log(arr.lenght);
+arr.shift();
+arr.pop();
+
+console.log(arr);*/
+
+
+/*2 
+let arr = ['orange', 'banana', 'pear'];
+console.log(arr.lenght);
+arr.push('apple', 'pinaple');
+arr.unshift('watermalon');
+console.log(arr.lenght);
+arr.splice(2, 0, 'mango');
+arr.shift();
+arr.pop();
+
+console.log(arr.lenght);*/
+
+/*3
+let arr = [1, 2, 3, 4, 5];
+arr.splice(3, 0, "a", "b", "c");
+
+console.log(arr);*/
+
+
+
+/*4
+let arr = [1, 2, 3, 4, 5];
+let sum = 0;
+let arr1 = arr.forEach( item => {
+    return sum += item
+})
+
+console.log(sum)*/
+
+
+
+/*5
+for(let i = 100; i > 0; i--){
+    console.log(i)
+}*/
